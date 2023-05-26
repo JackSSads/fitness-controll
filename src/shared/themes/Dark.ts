89ff1e84aacd227aaa0c70c9c0950f6d06ef97b2
JackSSads <fotosjackson2018@hotmail.com>
2023-05-26 20:34:06 -0300

    feat: Criando modo Dark

---
+++ src/shared/themes/Dark.ts
@@ -0,0 +1,25 @@
+import { createTheme } from "@mui/material";
+import { cyan, yellow } from "@mui/material/colors";
+
+export const DarkTheme = createTheme({
+    palette: {
+        primary: {
+            main: yellow[700],
+            dark: yellow[800],
+            light: yellow[500],
+            contrastText: "#FFFFFF",
+        },
+        
+        secondary: {
+            main: cyan[500],
+            dark: cyan[400],
+            light: cyan[300],
+            contrastText: "#FFFFFF",
+        },
+        
+        background: {
+            default: "#202124",
+            paper: "#303234",
+        },
+    },
+});
