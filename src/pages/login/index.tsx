20226b089a5a19c7f7e8738264b5faee76845410
JackSSads <fotosjackson2018@hotmail.com>
2023-05-26 20:36:43 -0300

    feat: testando troca de temas

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,12 @@
-import { Button } from "@mui/material"
+import { Button } from "@mui/material";
+
+import { useAppThemeContext } from "../../shared/contexts";
 
 export const Login = () => {
+
+    const { toggleTheme } = useAppThemeContext();
+
     return (
-        <Button variant="contained" fullWidth>Login</Button>
+        <Button variant="contained" color="primary" fullWidth onClick={toggleTheme}>Toggle</Button>
     );
 }; 
