9e592ab02e8ee7a3809e8511a6c029e55af25108
JackSSads <fotosjackson2018@hotmail.com>
2023-05-27 17:43:12 -0300

    feat: excluindo componente Home

--- src/shared/routes/index.tsx
+++ src/shared/routes/index.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route, Navigate} from "react-router-dom"
 import { Login } from "../../pages/login";
+import { Home } from "../../pages/home";
 
 export const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/login" element={<Login />}/>
+            <Route path="/" element={<Home/>}/>
 
             <Route path="*" element={<Navigate to={"/login"} />}/>
         </Routes>
