2041e809e53c49f4059cdceb5bd46d1f227aba39
JackSSads <fotosjackson2018@hotmail.com>
2023-05-27 21:52:07 -0300

    feat: agrupando método de navegação em um só lugar

--- src/shared/routes/index.tsx
+++ src/shared/routes/index.tsx
@@ -1,12 +1,28 @@
 import { Routes, Route, Navigate} from "react-router-dom"
 import { Login } from "../../pages/login";
-import { Home } from "../../pages/home";
+import { App } from "../../App";
+import { useAppDrawerContext } from "../contexts";
+import { useEffect } from "react";
 
 export const AppRoutes = () => {
+
+    const { setDrawerOption } = useAppDrawerContext();
+
+    useEffect(() => {
+
+        setDrawerOption([
+            {
+                label: "Página Inicial",
+                icon: "home",
+                path: "/login"
+            },
+        ]);
+    }, []);
+
     return (
         <Routes>
             <Route path="/login" element={<Login />}/>
-            <Route path="/" element={<Home/>}/>
+            <Route path="/" element={<App/>}/>
 
             <Route path="*" element={<Navigate to={"/login"} />}/>
         </Routes>
