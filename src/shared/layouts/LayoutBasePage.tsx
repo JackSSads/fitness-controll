cd38301790431889a7fcbc0b42d9964b3a28922e
JackSSads <fotosjackson2018@hotmail.com>
2023-05-31 17:05:49 -0300

    feat: configurando children e abertura do Drawer

--- src/shared/layouts/LayoutBasePage.tsx
+++ src/shared/layouts/LayoutBasePage.tsx
@@ -1,15 +1,42 @@
-import { Box } from "@mui/material";
+import { Box, Typography, useTheme, IconButton, useMediaQuery, Theme } from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
+import { useAppDrawerContext } from "../contexts";
 
 interface ILayoutBasePagesProps {
     children: React.ReactNode;
+    title: string;
 };
 
-export const LayoutBasePages: React.FC<ILayoutBasePagesProps> = ({ children }) => {
+export const LayoutBasePages: React.FC<ILayoutBasePagesProps> = ({ children, title }) => {
+
+    const smDown = useMediaQuery((theme: Theme) => theme.breakpoints.down("sm"));
+    const theme = useTheme();
+
+    const { toggleDrawerOpen } = useAppDrawerContext();
+
     return (
-        <Box>
-            Teste
-            
-            {children}
+        <Box height={"100%"} display={"flex"} flexDirection={"column"} gap={1}>
+            <Box display={"flex"} alignItems={"center"} padding={1} height={theme.spacing(12)} gap={1}>
+
+                {smDown && (
+                    <IconButton onClick={toggleDrawerOpen}>
+                        <MenuIcon />
+                    </IconButton>
+                )}
+
+                <Typography variant="h5" gutterBottom>
+                    {title}
+                </Typography>
+            </Box>
+
+            <Box>
+                Barra de Ferramentas
+            </Box>
+
+            <Box>
+                {children}
+            </Box>
         </Box>
     );
-;}
+    ;
+}
