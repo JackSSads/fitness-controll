50672e0f5045946985d6840b6eebc9afbd4dfe2f
JackSSads <fotosjackson2018@hotmail.com>
2023-05-31 17:48:16 -0300

    feat: adicionando paâmetro toobar

--- src/pages/dashbord/Dashboard.tsx
+++ src/pages/dashbord/Dashboard.tsx
@@ -2,7 +2,7 @@ import { LayoutBasePages } from "../../shared/layouts";
 
 export const Dashboard = () => {
     return(
-        <LayoutBasePages title="Dashboard" >
+        <LayoutBasePages title="Dashboard" toobar={<>Barra de Ferramentas</>} >
             Testando
         </LayoutBasePages>
     );
