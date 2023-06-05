12cafbcb64b8ed17ab1477fe76df9950cc1d000a
JackSSads <fotosjackson2018@hotmail.com>
2023-06-05 17:12:18 -0300

    feat: adicionando variaveis de ambiente

--- src/shared/services/api/axiosConfig/index.ts
+++ src/shared/services/api/axiosConfig/index.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 import { errorInterceptor, responseInterceptor } from "./interceptors";
+import { Enviroment } from "../../../environment";
 
 const API = axios.create({
-    baseURL: "http://localhost:3333"
+    baseURL: Enviroment.URL_BASE,
 });
 
 API.interceptors.response.use(
