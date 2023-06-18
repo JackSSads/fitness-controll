36fc05f86649591a8bf8939de7355aaf5d905be4
JackSSads <fotosjackson2018@hotmail.com>
2023-06-17 22:00:51 -0300

    feat: enviando dados para o DB

--- src/pages/pessoas/ListagemPessoas.tsx
+++ src/pages/pessoas/ListagemPessoas.tsx
@@ -1,23 +1,42 @@
-import { useMemo, useEffect } from "react";
+import { useMemo, useEffect, useState } from "react";
 
 import { useSearchParams } from "react-router-dom";
 
 import { ListingTools } from "../../shared/components";
 import { LayoutBasePages } from "../../shared/layouts";
 
-import { PessoaService } from "../../shared/services/api/pessoas/PessoasService";
+import { IListagemPessoa, PessoaService } from "../../shared/services/api/pessoas/PessoasService";
+import { useDebounce } from "../../shared/hooks";
 
 export const ListagemPessoas: React.FC = () => {
 
     const [searchParams, setSearchParams] = useSearchParams();
+    const { debounce } = useDebounce();
+
+    const [rows , setRows] = useState<IListagemPessoa[]>([]);
+    const [totalCount  , setTotalCount ] = useState(0);
 
     const busca = useMemo(() => {
         return searchParams.get("busca") || "";
     }, [searchParams]);
 
+
     useEffect(() => {
-        PessoaService
-    }, []);
+        debounce(() => {
+            PessoaService.getAll(1, busca)
+                .then((result) => {
+                    if (result instanceof Error) {
+                        alert(result.message);
+                        return;
+                    } else {
+                        console.log(result);
+
+                        setTotalCount(result.totalCount);
+                        setRows(result.data);
+                    };
+                });
+        });
+    }, [busca]);
 
     return (
         <LayoutBasePages
@@ -30,7 +49,7 @@ export const ListagemPessoas: React.FC = () => {
                     whenChangingSearchText={text => setSearchParams({ busca: text }, { replace: true })}
                 />
             }>
-
+                Pessoas
         </LayoutBasePages>
-    )
-}
+    );
+};
