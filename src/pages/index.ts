6810cdbeb7936bdb3a28611f351bc71b7a2081e7
JackSSads <fotosjackson2018@hotmail.com>
2023-07-08 21:54:12 -0300

    feat: exportando página de Academia

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,5 @@
+export * from "./academias/ListagemAcademias";
+export * from "./academias/AcademyDetails";
 export * from "./pessoas/ListagemPessoas";
 export * from "./pessoas/PersonsDetails";
 export * from "./dashbord/Dashboard";
